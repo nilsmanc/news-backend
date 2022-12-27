a03f3bbc8f3f7eeede57e5707eec10dbede27949
nilsmanc <etereaiberia@gmail.com>
2022-12-27 11:33:24 +0300

    atob

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -37,7 +37,8 @@ export class AuthService {
     const base64Url = token.split('.')[1];
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(
-      atob(base64)
+      window
+        .atob(base64)
         .split('')
         .map(function (c) {
           return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
